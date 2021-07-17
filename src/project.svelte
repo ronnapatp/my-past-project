6b509ce8ce8279ba8e44b1e9f5b1a2322a8c1172
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-07-17 20:46:07 +0700

    first upload

---
+++ src/project.svelte
@@ -0,0 +1,53 @@
+<script>
+    import logo from './assets/svelte.png'
+    import Counter from './lib/Counter.svelte'
+  </script>
+
+<main>
+<h1>Hello</h1>
+</main>
+<style>
+    :root {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+  
+    main {
+      text-align: center;
+      padding: 1em;
+      margin: 0 auto;
+      color: lightsalmon ;
+    }
+  
+    img {
+      height: 16rem;
+      width: 16rem;
+    }
+  
+    h1 {
+      color: lightsalmon;
+      text-transform: uppercase;
+      font-size: 4rem;
+      font-weight: 100;
+      line-height: 1.1;
+      margin: 2rem auto;
+      max-width: 14rem;
+    }
+  
+    p {
+      max-width: 14rem;
+      margin: 1rem auto;
+      line-height: 1.35;
+    }
+  
+    @media (min-width: 480px) {
+      h1 {
+        max-width: none;
+      }
+  
+      p {
+        max-width: none;
+      }
+    }
+  </style>
+  
